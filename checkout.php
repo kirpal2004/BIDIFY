0cbeb6f62fcdb3bde71748105469b720a3652a67
kirpalsolanki2004@gmail.com <kirpalsolanki2004@gmail.com>
2025-04-09 16:58:57 +0530

    Update checkout process handling

--- checkout.php
+++ checkout.php
@@ -13,7 +13,9 @@ $name = $_SESSION['username'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/checkout.css">
+    <!-- <link rel="stylesheet" href="./css/checkout.css"> -->
+    <link rel="stylesheet" href="./css/checkout.css?v=2">
+
 </head>
 <body>
 
