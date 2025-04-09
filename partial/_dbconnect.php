8890366c053c4a9218f5213ccb3dec081f605c11
kirpalsolanki2004@gmail.com <kirpalsolanki2004@gmail.com>
2025-04-09 13:28:33 +0530

    Updated database connection file

--- partial/_dbconnect.php
+++ partial/_dbconnect.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $username = "root";
     $password = "";
-    $database = "auction";
+    $database = "bid";
 
     $conn = mysqli_connect($server,$username,$password,$database);
     if($conn){
